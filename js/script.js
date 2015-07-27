b7c383a36b0f684e4ff8af5ab098d28769a4baef
JadeTiger88 <jadetiger88@gmail.com>
2015-07-26 20:01:35 -0700

    Add update and delete functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,53 @@
 $(document).one('pageinit', function() {
-	$('#submit').on('tap', addRun);
+
+	showRuns(); 
+
+	$('#submit-x').on('tap', addRun);
+
+	$('.delete-run').on('tap', deleteRun); 
+
+	$('.edit-run').on('tap', editRun);
+
+    function showRuns() {
+
+    	var runs = getRuns(); 
+
+         var len = runs.length;
+    	for (var i = 0; i < runs.length; i++) {
+    		$('#stats').append(
+    				'<li class="ui-body-inhereit ui-li-static"' + 'data-index="' + i + '"' + ' >' + 
+    			    	'<strong>Date: </strong>' + runs[i]["date"] + '<br>' +
+    					'<strong>Distance: </strong>' + runs[i]["mile"] + ' mile(s)' + 
+    					'<div class="controls">' + 
+    					'<a href="#add" class="edit-run">Edit</a> | <a class="delete-run" href="#">Delete</a>' + 
+    					'</div>' + 
+    				'</li>');
+    	}
+
+    	$('#home').bind('pageinit', function () {
+    		$('#stats').listview('refresh');
+    	});
+    }
+
+    function editRun() {
+    	var parent = $(this).parent(); 
+    	var grandParent = parent.parent(); 
+		var i = $(this).parent().parent().data("index"); 
+    	var runs = getRuns(); 
+    	 $('#add-mile').val(runs[i]["mile"]);
+    	 $('#add-date').val(runs[i]["date"]);
+		 // $("#submit-x").data("edit-index", i);
+		 $("#submit-x").attr("data-index", i.toString());
+    }
 
 	// Add Run Data
 	function addRun() {
 
+		var op = $(this).data("op");
+
 		// Get user inputs
-		var miles = $('#addMile').val(); 
-		var date = $('#addDate').val();
+		var miles = $('#add-mile').val(); 
+		var date = $('#add-date').val();
 
 		// Turn the user input into the run object
 		var run = {
@@ -14,8 +55,16 @@ $(document).one('pageinit', function() {
 			mile: parseFloat(miles)
 		};
 
-        // add the current run to the existing runs in the local storage
+        // If this call comes from edit then delete the old one 
+        // before adding the current run to the existing runs in the local storage
 		var runs = getRuns(); 
+		var index = $(this).data("index"); 
+		if (index != null) {
+			runs.splice(index, 1); 			
+		} 
+
+
+        // add the current run to the existing runs in the local storage
 		runs.push(run);			
 		localStorage.setItem('runs', JSON.stringify(runs));
 
@@ -27,7 +76,18 @@ $(document).one('pageinit', function() {
 	};
 
 
-     // Get runs from local storage
+    function deleteRun() {	
+
+    	var parent = $(this).parent(); 
+    	var grandParent = parent.parent(); 
+		var i = $(this).parent().parent().data("index"); 
+		var runs = getRuns(); 
+		runs.splice(i, 1); 
+		localStorage.setItem('runs', JSON.stringify(runs));
+		window.location.href="index.html";
+	}
+
+ // Get runs from local storage
 	function getRuns() {
 
 		var runs = new Array(); 
@@ -44,5 +104,6 @@ $(document).one('pageinit', function() {
 		// Sort by miles  
 		// return(runs.sort(function(a,b){return b.mile - a.mile}));
 	}
+
 });
 
